10e282bb956edfe4b10dcd482d070c587e5546be
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2022-03-06 02:20:21 -0500

    Java 17

--- src/main/webapp/run.bat
+++ src/main/webapp/run.bat
@@ -2,17 +2,18 @@
 
 :: delete certificate class and jar files
 del devKeys
+del appendix\*.class
 del jars\*.jar
 
 :: 2) gather platform (OS) specific jars from the OpenJFX libraries.
-curl.exe --output jars\javafx-base-16-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-base/16/javafx-base-16-win.jar
-curl.exe --output jars\javafx-controls-16-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-controls/16/javafx-controls-16-win.jar
-curl.exe --output jars\javafx-fxml-16-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/16/javafx-fxml-16-win.jar
-curl.exe --output jars\javafx-graphics-16-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/16/javafx-graphics-16-win.jar
+curl.exe --output jars\javafx-controls-17-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-controls/17/javafx-controls-17-win.jar
+curl.exe --output jars\javafx-base-17-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-base/17/javafx-base-17-win.jar
+curl.exe --output jars\javafx-fxml-17-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/17/javafx-fxml-17-win.jar
+curl.exe --output jars\javafx-graphics-17-win.jar --url https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/17/javafx-graphics-17-win.jar
 
 :: 1) package the JavaFX application in a jar
 :: compile
-::javac --module-path jars --add-modules javafx.controls, javafx.fxml ..\java\appendix\ToggleButtonDemo.java
+javac --module-path jars --add-modules javafx.controls,javafx.fxml appendix\ToggleButtonDemo.java
 
 :: create jar file
 jar cfm jars\ToggleButtonDemo.jar MyMan.txt appendix\*.class
@@ -22,16 +23,16 @@ jar cfm jars\ToggleButtonDemo.jar MyMan.txt appendix\*.class
 keytool -genkeypair -keyalg DSA -alias devName -keystore devKeys
 
 :: sign jar file
-jarsigner -keystore devKeys jars\javafx-base-16-win.jar devName
-jarsigner -keystore devKeys jars\javafx-controls-16-win.jar devName
-jarsigner -keystore devKeys jars\javafx-fxml-16-win.jar devName
-jarsigner -keystore devKeys jars\javafx-graphics-16-win.jar devName
+jarsigner -keystore devKeys jars\javafx-base-17-win.jar devName
+jarsigner -keystore devKeys jars\javafx-controls-17-win.jar devName
+jarsigner -keystore devKeys jars\javafx-graphics-17-win.jar devName
+jarsigner -keystore devKeys jars\javafx-fxml-17-win.jar devName
 
 :: sign project jar file
 jarsigner -keystore devKeys jars\ToggleButtonDemo.jar devName
 
 :: start java web start application
-::javaws ToggleButtonDemo.jnlp
+javaws ToggleButtonDemo.jnlp
 
 ::Test
 ::run ToggleButtonDemo file
